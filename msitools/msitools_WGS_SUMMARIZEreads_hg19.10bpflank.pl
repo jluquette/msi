77c41b0681458cb98d9317d156f8ee46d43db86a
Lovelace j Luquette <ljl11@clarinet000-207.orchestra>
2013-07-15 18:55:03 -0400

    Major overhaul of msitools by Joe:   - use strict and warning   - no longer re-write the input file to per-chrom intermediary files   - read the entire repeat library into memory instead of per-chrom   - alter indexes when parsing input file to account for new mapQ column   - add flank_bp parameter; specifies how many bp flanking each side of     STR must match the reference sequence.   - general code readability cleanup   - hopefully no bugs!  UNTESTED!

--- msitools/msitools_WGS_SUMMARIZEreads_hg19.10bpflank.pl
+++ msitools/msitools_WGS_SUMMARIZEreads_hg19.10bpflank.pl
@@ -1,3 +1,5 @@
+#!/usr/bin/env perl
+
 # TODO:
 #  make flank_size a command line arg
 #  use a better perl argument parsing library
@@ -5,177 +7,182 @@
 #  since I added the mapQ value to the line, everything needs to shift over
 #  by one.)
 
-chomp $ARGV[0];
-$filename = $ARGV[0];
-chomp $ARGV[1];
-$realfile = $ARGV[1];
+use strict;
+use warnings;
+use Getopt::Long;
 
-@chrarray = qw(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 X Y);
-#@chrarray = qw(10 Y);
+my $flank_bp = 2;
+my $short_test = 0;
+undef my $inputfile;
+undef my $outprefix;
+undef my $resource_path;
+GetOptions("flank_bp:i" => \$flank_bp,
+           "input=s" => \$inputfile,
+           "outprefix=s" => \$outprefix,
+           "resource_path=s" => \$resource_path,
+           "test" => \$short_test)
+    or die("error parsing arguments");
 
-print "$filename ..\n";
-open (output0, ">SPUTNIK_".$filename."_".$chrarray[0].".txt");
-open (output1, ">SPUTNIK_".$filename."_".$chrarray[1].".txt");
-open (output2, ">SPUTNIK_".$filename."_".$chrarray[2].".txt");
-open (output3, ">SPUTNIK_".$filename."_".$chrarray[3].".txt");
-open (output4, ">SPUTNIK_".$filename."_".$chrarray[4].".txt");
-open (output5, ">SPUTNIK_".$filename."_".$chrarray[5].".txt");
-open (output6, ">SPUTNIK_".$filename."_".$chrarray[6].".txt");
-open (output7, ">SPUTNIK_".$filename."_".$chrarray[7].".txt");
-open (output8, ">SPUTNIK_".$filename."_".$chrarray[8].".txt");
-open (output9, ">SPUTNIK_".$filename."_".$chrarray[9].".txt");
-open (output10, ">SPUTNIK_".$filename."_".$chrarray[10].".txt");
-open (output11, ">SPUTNIK_".$filename."_".$chrarray[11].".txt");
-open (output12, ">SPUTNIK_".$filename."_".$chrarray[12].".txt");
-open (output13, ">SPUTNIK_".$filename."_".$chrarray[13].".txt");
-open (output14, ">SPUTNIK_".$filename."_".$chrarray[14].".txt");
-open (output15, ">SPUTNIK_".$filename."_".$chrarray[15].".txt");
-open (output16, ">SPUTNIK_".$filename."_".$chrarray[16].".txt");
-open (output17, ">SPUTNIK_".$filename."_".$chrarray[17].".txt");
-open (output18, ">SPUTNIK_".$filename."_".$chrarray[18].".txt");
-open (output19, ">SPUTNIK_".$filename."_".$chrarray[19].".txt");
-open (output20, ">SPUTNIK_".$filename."_".$chrarray[20].".txt");
-open (output21, ">SPUTNIK_".$filename."_".$chrarray[21].".txt");
-open (outputX, ">SPUTNIK_".$filename."_".$chrarray[22].".txt");
-open (outputY, ">SPUTNIK_".$filename."_".$chrarray[23].".txt");
-#open (data, "SPUTNIKresult_exome_len5_".$filename.".txt") or die;
-open (data, $realfile) or die;
-while ($line = <data>) {
-@element = split(" ", $line); 
-$element[2] =~ s/chr//g; $element[2] = "chr".$element[2];
-if ($element[2] eq "chr".$chrarray[0]) {
-	print output0 $line;
-} elsif ($element[2] eq "chr".$chrarray[1]) {
-	print output1 $line;
-} elsif ($element[2] eq "chr".$chrarray[2]) {
-	print output2 $line;
-} elsif ($element[2] eq "chr".$chrarray[3]) {
-	print output3 $line;
-} elsif ($element[2] eq "chr".$chrarray[4]) {
-	print output4 $line;
-} elsif ($element[2] eq "chr".$chrarray[5]) {
-	print output5 $line;
-} elsif ($element[2] eq "chr".$chrarray[6]) {
-	print output6 $line;
-} elsif ($element[2] eq "chr".$chrarray[7]) {
-	print output7 $line;
-} elsif ($element[2] eq "chr".$chrarray[8]) {
-	print output8 $line;
-} elsif ($element[2] eq "chr".$chrarray[9]) {
-	print output9 $line;
-} elsif ($element[2] eq "chr".$chrarray[10]) {
-	print output10 $line;
-} elsif ($element[2] eq "chr".$chrarray[11]) {
-	print output11 $line;
-} elsif ($element[2] eq "chr".$chrarray[12]) {
-	print output12 $line;
-} elsif ($element[2] eq "chr".$chrarray[13]) {
-	print output13 $line;
-} elsif ($element[2] eq "chr".$chrarray[14]) {
-	print output14 $line;
-} elsif ($element[2] eq "chr".$chrarray[15]) {
-	print output15 $line;
-} elsif ($element[2] eq "chr".$chrarray[16]) {
-	print output16 $line;
-} elsif ($element[2] eq "chr".$chrarray[17]) {
-	print output17 $line;
-} elsif ($element[2] eq "chr".$chrarray[18]) {
-	print output18 $line;
-} elsif ($element[2] eq "chr".$chrarray[19]) {
-	print output19 $line;
-} elsif ($element[2] eq "chr".$chrarray[20]) {
-	print output20 $line;
-} elsif ($element[2] eq "chr".$chrarray[21]) {
-	print output21 $line;
-} elsif ($element[2] eq "chr".$chrarray[22]) {
-	print outputX $line;
-} elsif ($element[2] eq "chr".$chrarray[23]) {
-	print outputY $line;
-} 
+die "--input is required" if not defined $inputfile;
+die "--outprefix is required" if not defined $outprefix;
+die "--resource_path is required" if not defined $resource_path;
+
+print "flank_bp=$flank_bp\n";
+print "input=$inputfile\n";
+print "output prefix=$outprefix\n";
+
+# Build a hash of accepted chromosomes
+my @chrarray = qw(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 X Y);
+if ($short_test) {
+    @chrarray = qw(1 Y);
 }
-close data; close output0; close output1; close output2; close output3; close output4;
-close output5; close output6; close output7; close output8; close output9;
-close output10; close output11; close output12; close output13; close output14;
-close output15; close output16; close output17; close output18; close output19;
-close output20; close output21; close outputX; close outputY;
-
-
-# Read chromosome fasta sequences
-@chrarray = qw(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 X Y);
-#@chrarray = qw(10 Y);
-$chrSeq = (); 
-foreach $chr (@chrarray) {
-	print "reading chromosome $chr .. \n";
-	open (data, "/scratch/ljl11/neuron/msitools/hg19_chromosome_seq/chr".$chr.".fa") or die;
-	$null = <data>;	while ($line = <data>) {chomp $line; $chrSeq{"chr".$chr} .= $line;}
-	close data; 
+my %chrhash;
+foreach my $chr (@chrarray) {
+    $chrhash{$chr} = 1;
 }
+print "Using " . scalar(keys %chrhash) . " chromosomes...\n";
 
-open (output, ">SUMMARY_wgsCalls_".$filename.".txt");
-print output "index\tchr\tstart\tend\trepArray\n";
-open (SUPPREADS, ">SUPPORT_reads_".$filename.".txt");
+
+
+# Reads the full set of human reference sequences into memory
+print "Reading chromosome sequences into RAM..\n";
+my %chrSeq;
+foreach my $chr (keys %chrhash) {
+	print "reading chromosome $chr.. ";
+	open (F, "<", "$resource_path/hg19_chromosome_seq/chr$chr.fa") or die;
+	my $null = <F>;  # strip off the first >chrXXX line
+    my $nbytes = 0;
+    while (<F>) {
+        my $line = $_;
+        chomp $line;
+        $chrSeq{"chr$chr"} .= $line;
+        $nbytes += length($line);
+    }
+    print "$nbytes bytes.\n";
+	close F; 
+}
+print "done.\n";
+
+
+
+# Build a record of all known repeats on this chromosome
+# This used to be done one chrom at a time.  Changed by Joe.
+print "Building repeat index..\n";
+my @repeatChrom = ();
+my @repeatStart = ();
+my @repeatEnd = ();
+my @repeatSeq = ();
+my @repeatFlank1 = ();
+my @repeatFlank2 = ();
+my @repeatType = ();
+my $repeatno = 0;
+open (F, "<", "$resource_path/WGRef_7892585MS_withGENEcategory_FINAL_withMSTYPE_hg19.txt") or die;
+while (<F>) {
+    my $line = $_;
+	chomp $line;
+    my @element = split("\t", $line); 
+    my $chr = $element[0];
+    $chr =~ s/chr//g;  # Get rid of 'chr' if it's there
+    # Only load repeats in the specified chromosome list
+    if (exists $chrhash{$chr}) {
+	    $element[5] =~ s/\s//g;
+        $repeatChrom[$repeatno] = $element[0];
+        $repeatStart[$repeatno] = $element[1]; 
+	    $repeatEnd[$repeatno] = $element[2];
+        $repeatSeq[$repeatno] = $element[4];
+	    $repeatType[$repeatno] = $element[5];
+	    $repeatFlank1[$repeatno] =
+            uc(substr($chrSeq{"chr".$chr}, $element[1]-$flank_bp-1, $flank_bp));
+	    $repeatFlank2[$repeatno] =
+            uc(substr($chrSeq{"chr".$chr}, $element[2] , $flank_bp));
+	    ++$repeatno;
+    }
+}
+close F;
+print "done.\n";
+
+
+
+open (SUPPREADS, ">", "$outprefix.supporting_reads.txt");
 print SUPPREADS "chr\tstart\tend\tSTRlen\treadinfo\n";
 
-$flank_size = 10;
+# This used to be done per chromosome.  Changed by Joe.
+print "Reading input data..\n";
+open (F, "<", $inputfile) or die;
+my $nread = 0;
+my $indexPos = 0;
+my @lenArray = ();
+while (<F>) {
+    ++$nread;
+    if ($nread % 10000 == 0) {
+        print "$nread lines processed\n";
+    }
 
-foreach $chr (@chrarray) {
-	@repeatStart = (); @repeatEnd = (); @repeatSeq = (); @repeatFlank1 = (); @repeatFlank2 = (); @repeatType = (); $repeatno = 0;
-	open (data, "/scratch/ljl11/neuron/msitools/WGRef_7892585MS_withGENEcategory_FINAL_withMSTYPE_hg19.txt") or die;
-	while ($line = <data>) {
-		chomp $line; @element = split("\t", $line); 
-		if ($element[0] eq "chr".$chr) {
-			$element[5] =~ s/\s//g;
-			$repeatType[$repeatno] = $element[5]; $repeatStart[$repeatno] = $element[1]; 
-			$repeatEnd[$repeatno] = $element[2];	$repeatSeq[$repeatno] = $element[4];
-			$repeatFlank1[$repeatno] = uc(substr($chrSeq{"chr".$chr}, $element[1] - ($flank_size+1), $flank_size));
-			$repeatFlank2[$repeatno] = uc(substr($chrSeq{"chr".$chr}, $element[2] , $flank_size));
-			++$repeatno;
-		}
-	}
-	close data;
+	chomp;
+    my @element = split(" "); 
+    my $chrom = $element[2];
+    $chrom =~ s/chr//g;  # Get rid of 'chr' if it's there
 
-	print "Chr $chr - $filename - WGS .. \n";	@foundRead = (); 
-	open (data, "SPUTNIK_".$filename."_".$chr.".txt") or die;
-	$indexPos = 0; @lenArray = (); $flag_direction = "forward";
-	while (<data>) {
-		chomp; @element = split(" "); 
-		$startRepeat = $element[3] + $element[9]; $endRepeat = $element[3] + $element[10]; 
-		$flag_direction = ""; # searching direction
-		while (1) { # searching overlapping repeat from indexPos
-			#print "$indexPos - $flag_direction for $startRepeat to $endRepeat VS $repeatStart[$indexPos] to $repeatEnd[$indexPos] "; $null = <STDIN>; 
+    # Skip if not one of our requested chromosomes
+    next if not exists $chrhash{$chrom};
 
-			if ($endRepeat >= $repeatStart[$indexPos] and $startRepeat <= $repeatEnd[$indexPos]) {
-				if ($element[8] eq $repeatType[$indexPos]) {
-	                #print "flank1 : $repeatFlank1[$indexPos] and flank2 : $repeatFlank2[$indexPos] \n";
-
-                #print substr($element[12],$element[9]-($flank_size+1),$flank_size)."::".substr($element[12],$element[10],$flank_size)."\n";
-					#print "read: ".substr($element[12],$element[9]-($flank_size+1),$element[10]-$element[9]+4)."\n"; # $null = <STDIN>;
-                    # joe: fix the case where $element[9]-3 is negative, which causes the substring to be taken from the end of the read
-					if ($repeatFlank1[$indexPos] eq substr($element[12],$element[9]-($flank_size+1),$flank_size) and $element[9]-($flank_size+1) >= 0 and $repeatFlank2[$indexPos] eq substr($element[12],$element[10],$flank_size)) {
-						$lenArray[$indexPos] .= scalar($element[10]-$element[9]+1).",";
-                        print SUPPREADS "$chr\t$repeatStart[$indexPos]\t$repeatEnd[$indexPos]\t".scalar($element[10]-$element[9]+1)."\t@element\n";
-					}
+	my $flag_direction = ""; # searching direction
+	my $startRepeat = $element[3] + $element[10];
+    my $endRepeat = $element[3] + $element[11]; 
+    my $this_flank1 = substr($element[13], $element[10]-$flank_bp-1, $flank_bp);
+    my $this_flank2 = substr($element[13], $element[11], $flank_bp);
+	while (1) { # search all repeats for an overlapping record
+		if ($endRepeat >= $repeatStart[$indexPos] and
+            $startRepeat <= $repeatEnd[$indexPos]) {
+			if ($element[9] eq $repeatType[$indexPos]) {
+                # joe: fix the case where $element[9]-$flank_bp-1 is
+                # negative, which causes the substring to be taken from
+                # the end of the read
+				if ($repeatFlank1[$indexPos] eq $this_flank1 and
+				    $repeatFlank2[$indexPos] eq $this_flank2 and
+                    $element[10] - $flank_bp - 1 >= 0) {
+                    my $replen = scalar($element[11] - $element[10] + 1);
+					if (exists $lenArray[$indexPos]) {
+                        $lenArray[$indexPos] .= ",$replen";
+                    } else {
+                        $lenArray[$indexPos] .= "$replen";
+                    }
+                    print SUPPREADS "$chrom\t$repeatStart[$indexPos]\t$repeatEnd[$indexPos]\t$replen\t@element\n";
 				}
-				last; # terminate since a read is allowed for a 'single' match
-			} elsif ($startRepeat > $repeatEnd[$indexPos]) { 
-				# Forward searching (Genome repeat << Read repeat) ----------------------------------
-				if ($flag_direction eq "backward" or $indexPos >= $repeatno) {last} else {
-					$flag_direction = "forward"; ++$indexPos;} 
-			} elsif ($endRepeat < $repeatStart[$indexPos]) {
-					# Backward searching (Read repeat >> Genome repeat) ---------------------------------
-				if ($flag_direction eq "forward" or $indexPos <= 0) {last} else {
-					$flag_direction = "backward"; --$indexPos;}
-			} else {
-				print "Failed! READ: $element[0] - $element[2] : $startRepeat - $endRepeat"; exit;
 			}
-			#if ($indexPos < 0) {++$indexPos} elsif ($indexPos >= $repeatno) {last}
+			last; # Don't allow a read to match multiple repeat records
+		} elsif ($startRepeat > $repeatEnd[$indexPos]) { 
+			# Forward searching (Genome repeat << Read repeat)
+			if ($flag_direction eq "backward" or $indexPos >= $repeatno) {
+                last;
+            } else {
+				$flag_direction = "forward";
+                ++$indexPos;
+            } 
+		} elsif ($endRepeat < $repeatStart[$indexPos]) {
+			# Backward searching (Read repeat >> Genome repeat)
+			if ($flag_direction eq "forward" or $indexPos <= 0) {
+                last;
+            } else {
+				$flag_direction = "backward";
+                --$indexPos;
+            }
+		} else {
+            print "ERROR: no repeat record found for read:\n";
+			die("$chrom:$element[0]-$element[2]: $startRepeat-$endRepeat");
 		}
 	}
-	close data;
-	for ($i = 0; $i < $repeatno; ++$i) {
-		print output $i."\t$chr\t$repeatStart[$i]\t$repeatEnd[$i]\t$lenArray[$i]\n";
-	}
-	#unlink "SPUTNIK_".$filename."_".$chr.".txt";
-} # End of chromosome loop
-close output;
+}
+close F;
+print "done.\n";
 
+print "Writing results to $outprefix.str_summary.txt.. ";
+open (OUTPUT, ">$outprefix.str_summary.txt");
+print OUTPUT "index\tchr\tstart\tend\trepArray\n";
+for (my $i = 0; $i < $repeatno; ++$i) {
+    if (exists $lenArray[$i]) {
+	    print OUTPUT "$i\t$repeatChrom[$i]\t$repeatStart[$i]\t$repeatEnd[$i]\t$lenArray[$i]\n";
+    }
+}
+close OUTPUT;
+print "done.\n";
