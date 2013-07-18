b1aed571742ae149a020e23db751725129737855
Lovelace j Luquette <ljl11@clarinet000-207.orchestra>
2013-07-17 22:20:58 -0400

    Increase msitools memory from 32G->52G

--- msitools/run.sh
+++ msitools/run.sh
@@ -1,4 +1,4 @@
 for i in ../sputnik/*.sputnik.txt.gz; do
     x=$(basename $i .rmdup.nomapq0.sputnik.txt.gz)
-    bsub -q short -W 12:00 -o $x/$x.log -R 'rusage[mem=32000]' "gunzip -c $i | ./msitools.pl --input /dev/stdin --outprefix $x/$x.flank10 --resource_path `pwd` --flank_bp 10"
+    bsub -q short -W 12:00 -o $x/$x.log -R 'rusage[mem=52000]' "gunzip -c $i | ./msitools.pl --input /dev/stdin --outprefix $x/$x.flank10 --flank_bp 10"
 done
